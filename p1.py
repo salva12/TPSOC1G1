0a71629505fe83dceae23941117bd925aba29eb3
salvador <salvamanu12@gmail.com>
2019-09-07 23:02:48 -0300

    esqueleto de interfaz hecho con tkinter, lo pueden usar para jugar un poco

---
+++ p1.py
@@ -0,0 +1,28 @@
+
+"""*import random
+
+print("ETO NO E "+"COCA")
+def func1(x,y,z):
+  global num
+  f=8
+  print(x**y+z)
+  num = 2
+
+func1(2,5,3)
+print(num)
+z=5+1j
+print(type(z))
+print(z)
+print(random.randrange(1,100000))"""
+print(float(5))
+print(str(5))
+a = '''Lorem ipsum dolor sit amet
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+print(a[6:11])
+print(len(a[6:11]))
+age = 36
+txt = "My name is John, and I am {},y tengo {} plata"
+print(txt.format(age,566))
---
+++ tk1.py
@@ -0,0 +1,10 @@
+from tkinter import*
+
+raiz = Tk()
+
+raiz.title("Administrador de memoria") 
+raiz.resizable(1,1)
+raiz.geometry("1000x600")
+raiz.config(bg="grey")
+raiz.iconbitmap("img2.ico")
+raiz.mainloop()
